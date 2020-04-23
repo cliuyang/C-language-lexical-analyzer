fead05b6eaf94cda0bbe36cc0729a27395197da8
cliuyang <cliuyang249@gmail.com>
2020-04-23 23:23:17 +0800

    调整代码  修改测试文件

--- test.c
+++ test.c
@@ -4,6 +4,21 @@
 
 int main()
 {
-    printf("hello world!\n");
+    auto break case char const continue default do double else enum extern float for goto if inline int long regisiter restrict return short singed sizeof static struct switch typedef union unsigned void volatile while _Alignas _Alignof _Atomic _Bool _Complex _Generic _Imaginary _Noreturn _Static_assert _Thread_local
+
+    ... ; , : ( ) [ ] { } < > . ? /* */ //
+
+    >>= <<= += -= *= /= %= &= ^= |= >> << ++ -- -> && || <= >= == != = & | ^ + - * / %
+
+    _asd132 asd132 123asd
+
+    12 012 12l 12L 12u 12U 
+
+    1e5f 15F 12.45 15.e23f
+
+    "hello world" 'a' '1' '@' '' '123' '\''
+
+    @$
+
     return 0;
 }
